c2deb370fd88a3b1ed6e139c1033634132541548
Pavan Srinivas <pavansrinivas017@gmail.com>
2020-11-04 19:31:16 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+echo "hello world ";
+
+
+
+
+?>
+
+------------------------------------------
+<style>
+body
+{
+background-color: blue;
+}
+
+h1
+{
+  text-align: center;
+  color: white;
+  border:5px solid yellow;
+}
+
+
+
+</style>
+-------------------------------------------
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<h1>i am child </h1>
+</body>
+</html>
